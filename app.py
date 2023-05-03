e08fd0a10bd9fdc182532fa0fd1b162cada5aad9
Huiping Miao <Huiping.Miao@sas.com>
2023-05-03 15:56:44 -0400

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from src.database import add_reminder, list_reminders
+from src.deadlined_reminders import DateReminder, DeadlinedReminder
+from src.external_reminders import EveningReminder
+from src.reminder import PoliteReminder
 
+DeadlinedReminder.register(PoliteReminder)
 
 def handle_input():
     choice = input("Choice: ")
@@ -12,7 +16,8 @@ def handle_input():
     elif(choice == "2"):
         print()
         reminder = input("What would you like to be reminded about?: ")
-
+        date = input('When is that due?:')
+        
         add_reminder(reminder)
         list_reminders()
     else:
